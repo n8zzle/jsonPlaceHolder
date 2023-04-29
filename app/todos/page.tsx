ff07ae5483b5af9605bb8464b1ee053ce18fdce7
n8zzle <playwithn8zzle@gmail.com>
2023-04-29 22:29:52 +0200

    data fetching done

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,9 +1,34 @@
+"use client";
 import React from "react";
-
+import { useState } from "react";
 type Props = {};
 
-const Todo = (props: Props) => {
-  return <div>Todo</div>;
+const getTodos = async () => {
+  const url = "https://jsonplaceholder.typicode.com/todos";
+  const res = await fetch(url);
+  return res.json();
+};
+
+const Todo = async (props: Props) => {
+  const data = await getTodos();
+  console.log(data);
+  return (
+    <div className="w-full">
+      <h1 className="text-4xl font-extrabold text-center p-10">
+        TODOS from API
+      </h1>
+      <div className="max-w-7xl mx-auto grid grid-cols-2 gap-4">
+        {data.map((d) => (
+          <div
+            key={d.id}
+            className="border-2 rounded-lg hover:border-indigo-600"
+          >
+            <p className="p-5">{d.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,41 @@
+"use client";
+import { Avatar } from "@mui/material";
 import React from "react";
 
 type Props = {};
 
-const Users = (props: Props) => {
-  return <div>Users</div>;
+const getUsers = async () => {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const res = await fetch(url);
+  return res.json();
+};
+
+const Users = async (props: Props) => {
+  const data = await getUsers();
+  console.log(data);
+  return (
+    <div className="w-full">
+      <h1 className="text-4xl font-extrabold text-center p-10">
+        Users from API
+      </h1>
+      <div className="max-w-7xl mx-auto  grid grid-cols-2 gap-4">
+        {data.map((d) => (
+          <div
+            key={d.id}
+            className="  border-2  border-rounded-lg items-center"
+          >
+            <div className="p-5 flex flex-row space-x-5">
+              <Avatar>{d.name[0]}</Avatar>
+              <div>
+                <h2 className="font-bold">{d.name}</h2>
+                <p className="text-sm text-gray-500">{d.email}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
